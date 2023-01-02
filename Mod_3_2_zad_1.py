649432095ac9402b7335f152b6b882afc104bcad
tojakarolina <karolina.przytula1997@gmail.com>
2023-01-02 21:47:28 +0100

    Init project, define dictionary

---
+++ Mod_3_2_zad_1.py
@@ -0,0 +1,10 @@
+shopping_list = {"piekarnia": ["chleb", "bułki", "pączki"],
+"warzywniak": ["marchew", "seler", "rukola"]}
+
+shops = list(shopping_list.keys())
+products = list(shopping_list.values())
+
+warzywniak_products = products[0]
+piekarnia_products = products[1]
+
+print('Lista zakupów')
