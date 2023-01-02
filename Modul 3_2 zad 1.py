c7ffff613da93bf68bbea6acb64278e825b6aed9
tojakarolina <karolina.przytula1997@gmail.com>
2023-01-02 18:52:12 +0100

    Init Project M3Z1

---
+++ Modul 3_2 zad 1.py
@@ -0,0 +1,14 @@
+shopping_list = {"piekarnia":["chleb", "bułki", "pączki"],
+"warzywniak":["marchew","seler", "rukola"]}
+
+shops = list(shopping_list.keys())
+products = list(shopping_list.values())
+warzywniak_products = products[0]
+piekarnia_products = products[1]
+for a in range(len(warzywniak_products)):
+        warzywniak_products[a] = warzywniak_products[a].capitalize()
+        piekarnia_products[a] = piekarnia_products[a].capitalize()
+products[0] = warzywniak_products
+products[1] = piekarnia_products
+        
+print("Lista zakupów")
