7e14b54a73fd42214032703d398201a361c4c29e
tojakarolina <karolina.przytula1997@gmail.com>
2023-01-02 21:55:14 +0100

    Init project, define dictionary and make shopping list

--- Mod_3_2_zad_1.py
+++ Mod_3_2_zad_1.py
@@ -8,3 +8,13 @@ warzywniak_products = products[0]
 piekarnia_products = products[1]
 
 print('Lista zakupów')
+
+for a in range(len(warzywniak_products)):
+        warzywniak_products[a] = warzywniak_products[a].capitalize()
+        piekarnia_products[a] = piekarnia_products[a].capitalize()
+products[0] = warzywniak_products
+products[1] = piekarnia_products
+
+for i in range(len(shops)):
+        print(f"Idę do {shops[i].capitalize()}, kupuję tu następujące rzeczy: {products[i]}.")
+
